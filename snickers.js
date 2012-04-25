7334a1dba858ac7b42b80d9c41acf3868993d728
Michael Scott Hertzberg <mshertzberg@gmail.com>
2012-04-25 14:31:19 -0400

    update header comments

--- snickers.js
+++ snickers.js
@@ -1,4 +1,13 @@
-/*jslint browser: true, css: false, maxerr: 50, indent: 4 */
+/**
+ * snickers.js
+ *
+ * I'm not sure what this does yet, but you
+ * always need to start small to improve.
+ *
+ * (c) 2012 moimikey
+ *
+ * jslint browser: true, css: false, maxerr: 50, indent: 4
+ */
 (function (window, console) {
 	"use strict";
 
