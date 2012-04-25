b929f29b40f0ca0808b2578db4e34dfb510deace
Michael Scott Hertzberg <mshertzberg@gmail.com>
2012-04-25 18:06:52 -0400

    updated comments

--- snickers.js
+++ snickers.js
@@ -1,12 +1,36 @@
 /**
  * snickers.js
  *
- * I'm not sure what this does yet, but you
- * always need to start small to improve.
+ * I'm not sure what this does yet, but you always need to start small to
+ * improve.
  *
- * (c) 2012 moimikey
+ * Copyright © 2012 Michael Scott Hertzberg (moimikey)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * $v: 0.1
+ * $a: moimikey
+ * $y: 2012
+ * $u: http://www.hertzberg.co
  *
  * jslint browser: true, css: false, maxerr: 50, indent: 4, predef: jQuery, $
+ *
  */
 var snickers = (function ($) {
 	"use strict";
