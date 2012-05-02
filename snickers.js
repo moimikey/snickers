5ed665bbcb61e9ecac81e2686745ac142e6111c0
Michael Scott Hertzberg <mshertzberg@gmail.com>
2012-05-02 18:10:15 -0400

    simple method to split comma delimited string to array

--- snickers.js
+++ snickers.js
@@ -77,6 +77,10 @@
 
 	};
 
+	snickers.csv2array = function (csv) {
+		return csv.split(',');
+	};
+
 	snickers.highlightJSON = function (json) {
 		json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 		return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
