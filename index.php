80b4f9681ab3c2353fa13cecc215765448567edf
Michael Scott Hertzberg <mshertzberg@gmail.com>
2012-05-02 15:01:30 -0400

    display our geoip data

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
     <script src="snickers.js"></script>
 	<style>
 		input.zipcode { display: block; padding: 5px; border: 1px solid #EEE; outline: none; border-radius: 4px }
+		pre { outline: 1px solid #ccc; padding: 5px; margin: 5px; }
+		.string { color: green; }
+		.number { color: darkorange; }
+		.boolean { color: blue; }
+		.null { color: magenta; }
+		.key { color: red; }
 	</style>
 </head>
 <body>
 <div class="test"><input type="text" placeholder="Enter IP Address" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" class="zipcode"></div>
+<div class="results"></div>
 </body>
 </html>
--- snickers.js
+++ snickers.js
@@ -77,6 +77,25 @@
 
 	};
 
+	snickers.highlineJSON = function (json) {
+		json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+		return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+			var cls = 'number';
+			if (/^"/.test(match)) {
+				if (/:$/.test(match)) {
+					cls = 'key';
+				} else {
+					cls = 'string';
+				}
+			} else if (/true|false/.test(match)) {
+				cls = 'boolean';
+			} else if (/null/.test(match)) {
+				cls = 'null';
+			}
+			return '<span class="' + cls + '">' + match + '</span>';
+		});
+	};
+
 	snickers.data = function () {
 		$(function () {
 			var zipcode = $('input.zipcode');
@@ -87,9 +106,8 @@
 					dataType: 'jsonp',
 					crossDomain: true,
 					success: function (data) {
-						if (data) {
-							console.log(data);
-						}
+						var json = JSON.stringify(data, undefined, 4);
+						$('.results').append('<pre></pre>').children().html(snickers.highlineJSON(json));
 					}
 				});
 			} else {
