9b0b7a131f6ad1a786cf41c5494c85ccb5145793
Michael Scott Hertzberg <mshertzberg@gmail.com>
2012-06-07 23:27:22 -0400

    start over

--- snickers.js
+++ snickers.js
@@ -24,11 +24,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
- * $v: 0.1
- * $a: moimikey
- * $y: 2012
- * $u: http://www.hertzberg.co
- *
  * jslint browser: true, regexp: true, css: false, maxerr: 50, indent: 4, predef: console, jQuery, navigator, window
  *
  */
@@ -42,127 +37,14 @@
 	 */
 	var snickers = x.snickers || {};
 
-	snickers.info = function () {
-
-	};
-
-	/**
-	 * Get browser information
-	 *
-	 * @return {Object}
-	 */
-	snickers.browser = function () {
-		var errorLog = {
-			browserEngine: navigator.product,
-			browserAgent: navigator.userAgent,
-			browserCookies: navigator.cookieEnabled,
-			browserArch: navigator.platform,
-			language: navigator.language,
-			urlCurrent: window.location.href,
-			urlCurrentHash: window.location.hash,
-			urlQuery: window.location.search,
-			urlCurrentHost: window.location.hostname,
-			urlReferer: window.document.referrer,
-			urlTitle: window.document.title
-		};
-
-		return errorLog;
-	};
-
-	snickers.map = function () {
-
-	};
-
-	snickers.markers = function () {
-
-	};
-
-	snickers.csv2array = function (csv) {
-		return csv.split(',');
-	};
-
-	snickers.highlightJSON = function (json) {
-		json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-		return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-			var cls = 'number';
-			if (/^"/.test(match)) {
-				if (/:$/.test(match)) {
-					cls = 'key';
-				} else {
-					cls = 'string';
-				}
-			} else if (/true|false/.test(match)) {
-				cls = 'boolean';
-			} else if (/null/.test(match)) {
-				cls = 'null';
-			}
-			return '<span class="' + cls + '">' + match + '</span>';
-		});
-	};
-
-	snickers.data = function () {
-		$(function () {
-			var zipcode = $('input.zipcode');
-
-			if ($(zipcode).val().match(/[\d\.\d\.\d\.\d]/)) {
-				$.ajax({
-					url: 'http://freegeoip.net/json/' + $(zipcode).val() + '?callback=?',
-					dataType: 'jsonp',
-					crossDomain: true,
-					success: function (data) {
-						var json = JSON.stringify(data, undefined, 4);
-						$('.results').prepend('<pre></pre>').children(':first').html(snickers.highlightJSON(json));
-					}
-				});
-			} else {
-				$(zipcode).css({
-					boxShadow: 'inset 0 0 10px 0 red'
-				});
-				return false;
-			}
-		});
-	};
-
-	snickers.humans = function () {
-		$(function () {
-			var index = 1, url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBMDu69iETLL9S7KMRYy2Yz-mq8O9rl2-g&cx=013036536707430787589:_pqjad5hr1a&q=inurl:humans.txt%20filetype:txt&alt=json&num=10&start=';
-
-			$.ajax({
-				url: url + index,
-				dataType: 'json',
-				crossDomain: true,
-				success: function (data) {
-					var json = JSON.stringify(data, undefined, 4);
-					$('.results').prepend('<pre></pre>').children(':first').html(snickers.highlightJSON(json));
-				}
-			});
-		});
-	}
-
-	/**
-	 * Event bindings
-	 *
-	 */
 	snickers.binds = function () {
 		$(function () {
-			var keyCode;
 
-			$('input.zipcode').on('keypress', function (e) {
-				keyCode = e.keyCode || e.which;
-
-				if (13 === keyCode) {
-					snickers.humans();
-				}
-			});
-
-			$('.clear').on('click', function () {
-				$('.results').empty();
-			});
 		});
 	};
 
 	/**
-	 * Initialize methods
+	 * Init
 	 *
 	 * @type {*}
 	 */
